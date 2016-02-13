985dfd6fb59ca90d3520d067923974e6496dc155
m57 <mitchell.hines@irmplc.com>
2016-02-13 18:01:33 +0000

    1.1: Added support for auto ARP scanning and fixed a few bugs.

--- vconfigurator.py
+++ vconfigurator.py
@@ -16,15 +16,17 @@ import sys
 import commands
 import time
 
-VERSION = "1.0"
+VERSION = "1.1"
+m	= False
+arp	= False
 
 def pc(c, str):
 	if c == 1:
-		print "\033[1;32m[+]\033[0m %s" % str
+		print "\033[1;32m[+]\033[0m %s" % str # Green
 	elif c == 2:
-		print "\033[1;31m[!]\033[0m %s" % str
+		print "\033[1;31m[!]\033[0m %s" % str # Red
 	elif c == 3:
-		print "\033[1;33m[?]\033[0m %s" % str
+		print "\033[1;33m[?]\033[0m %s" % str # Info
 	else:
 		print "wu w0t m8?!"
 
@@ -47,7 +49,7 @@ def init(iface, c):
 		pc(2, "Error opening file '%s'" % c)	
 		exit(1)
 
-def c_vlans(c,all):
+def c_vlans(c,all, make_flag):
 
 	configs = []
 
@@ -55,7 +57,8 @@ def c_vlans(c,all):
 
 	for line in f.readlines():
 		if len(line) > 1:
-			configs.append(line.split(","))
+			if not '#' in line[0]:
+				configs.append(line.split(","))
 
 	num_all_configs = len(configs)
 	num_exit 	= len(configs)+1
@@ -99,24 +102,36 @@ def c_vlans(c,all):
 			continue
 		time.sleep(0.5)
 
-		e = commands.getstatusoutput("ifconfig %s.%s %s netmask %s" % (iface, cnf[3], cnf[0], cnf[1]) )[1]
-		
+		if not (cnf[0].lower() == "dhcp"):
+			e = commands.getstatusoutput("ifconfig %s.%s %s netmask %s" % (iface, cnf[3], cnf[0], cnf[1]) )[1]
+		else:
+			os.system("dhclient %s.%s" % (iface, cnf[3]) )	
 		if e:
 			pc(2, e)
 			exit(1)
 		
 		time.sleep(0.5)
 
-		commands.getstatusoutput("route add default gw %s %s.%s" % (cnf[2], iface, cnf[3]))[1]
-		if "File exists" in e:
-			pc(3, e)
+		if " " in cnf[4]:
+			cnf[4] = cnf[4].strip().replace(" ", "_")
+		else:
+			cnf[4] = cnf[4].strip()
 
-		elif e:
-			pc(2, e + " - Skipping but will still configure IP...")
-			continue
+		# CHECK IF WE HAVE SET A GATEWAY
+		if (len(cnf[2]) > 0):
+			commands.getstatusoutput("route add default gw %s %s.%s" % (cnf[2], iface, cnf[3]))[1]
+			if "File exists" in e:
+				pc(3, e)
 
-		pc(1, "VID '%s' : '%s.%s ip:'%s' netmask:'%s' gateway:'%s' (Comment: '%s')" % (cnf[3], iface, cnf[3], cnf[0], cnf[1], cnf[2], cnf[4].strip()))
+			elif e:
+				pc(2, e + " - Skipping but will still configure IP...")
+				continue
 
+			pc(1, "VID '%s' : '%s.%s ip:'%s' netmask:'%s' gateway:'%s' (Comment: '%s')" % (cnf[3], iface, cnf[3], cnf[0], cnf[1], cnf[2], cnf[4]))
+		else:
+			pc(1, "VID '%s' : '%s.%s ip:'%s' netmask:'%s' gateway:'N/A' (Comment: '%s')" % (cnf[3], iface, cnf[3], cnf[0], cnf[1], cnf[4]))
+
+		
 	if ch != num_all_configs and ch != -1:
 		pc(3, "ifconfig %s.%s\n" % (iface, configs[0][3]) )
 		os.system("ifconfig %s.%s" % (iface, configs[0][3]) )
@@ -126,7 +141,36 @@ def c_vlans(c,all):
 	pc(3, "Routing table\n")
 	os.system("route -n")
 	
+	if (m):
+		for cnf in configs:
+	
 
+			try:
+				os.mkdir(cnf[4]) # create the VLAN directory for testing purposes.
+			except OSError:
+				pc(3, "Directory '%s' already exists. Skipping ARP scan. (Remove all VLAN folders to ARP scan, this is more of a safety feature to avoid losing any test data)" % cnf[4])
+
+			if (arp):
+				if ( len(commands.getstatusoutput("which arp-scan")[1]) > 1):
+					pc(1, "ARP Scanning VLAN '%s' using 'arp-scan'" % cnf[3])
+
+					os.system("arp-scan -l -I %s.%s > %s/arp-scan_live_hosts.txt" % ( iface, cnf[3], cnf[4] ))
+					os.system("cat %s/arp-scan_live_hosts.txt | grep -E '([0-9|a-f]{2}:){5}' | awk '{print $1}' > %s/live_ips.txt" % ( cnf[4], cnf[4]))
+
+					if ( os.path.getsize("%s/live_ips.txt" % cnf[4] ) == 0 ):
+						pc(3, "VLAN '%s' appears to be empty, no hosts returned from ARP scan." % cnf[3] )
+						os.system("touch %s/EMPTY_VLAN" % cnf[4] )
+
+				elif ( len(commands.getstatusoutput("which nmap")[1]) > 1):
+					pc(1, "ARP Scanning VLAN '%s' using 'nmap '" % cnf[3])
+
+					os.system("nmap -sP -n  %s -oN > %s/nmap-ARP-sP-n_live_hosts.txt" % (cnf[3], cnf[4] ))
+					os.system("cat %s/nmap-ARP-sP-n_live_hosts.txt | grep 'scan report for' | cut -d ' ' -f 5 > %s/live_ips.txt" % ( cnf[4], cnf[4] ))
+
+				else:
+					pc(2, "Could not find 'nmap' or 'arp-scan' binary in $PATH. Could not perform ARP scan.")
+
+		
 def rem_vlans(iface, all, id):
 	
 	if all:
@@ -138,7 +182,7 @@ def rem_vlans(iface, all, id):
 
 def usage():
 	
-		pc(2, "Usage: %s -i [interface] -c [network_config_file.txt] [options]" % sys.argv[0])
+		pc(2, "Usage: %s -i [interface] -c [network_config_file.csv] [options]" % sys.argv[0])
 		print "\nOptions:"
 		print "\tBy default, this tool will offer an interactive menu, to enable all VLANS use -a"
 		print ""
@@ -147,13 +191,16 @@ def usage():
 		print "\t-a\t\tAdd and configure all VLANS in provided file."
 		print "\t-d [vid]\tDelete specific VLAN ID."
 		print "\t-r\t\tRemove all virtual interfaces. (vconfig rem)"
+		print "\t-m\t\tCreate folders with the names of the VLANS. This creates in current directory (Useful for pentesting)."
+		print "\t--arp\t\tAutomatically ARP scan VLAN and output into 'ips.txt' in relevant VLAN folder (implies -m)" 
 		print 
-		print "An example configuration file (csv order: ip,netmask,gateway,vlanid,comment):\n"
+		print "An example configuration file (csv order: ip,netmask,gateway,vlanid,VLAN_Name):\n"
 		print "\t192.168.1.15,255.255.255.0,92.168.1.1,100,HOME_NETWORK"
 		print "\t192.168.2.15,255.255.255.0,192.168.2.254,200,MGMT_NETWORK"
 		print ""
 		print "# vconfigurator.py -i eth0 -c networks.csv (interactive mode)"
 		print "# vconfigurator.py -i eth0 -c networks.csv -a (auto-enable all vlans)"
+		print "# vconfigurator.py -i eth0 -c networks.csv -a --arp (auto-enable all vlans + ARP scan all vlans)"
 		print ""
 
 		exit(1)
@@ -161,7 +208,7 @@ def usage():
 if __name__ == "__main__":
 
 	print ""
-	print "                       ___ \033[1;32mVconfigurator v.%s\033[0m ___" % VERSION
+	print "                ___ \033[1;32mVconfigurator v.%s\033[0m ___ \033[1;32mby: \\x90\033[0m" % VERSION
 	print ""
 	print "   A simple utility to auto configure VLAN connectivity for pen testing. "
 	print ""
@@ -181,6 +228,13 @@ if __name__ == "__main__":
 		rem_vlans(iface,0,sys.argv[sys.argv.index("-d")+1])
 		exit(1)
 
+	if "-m" in sys.argv:
+		m = True
+
+	if "--arp" in sys.argv:
+		m = True
+		arp = True
+
 	if "-c" not in sys.argv:
 		usage()
 	else:
@@ -189,7 +243,7 @@ if __name__ == "__main__":
 	init(iface, cfile)
 
 	if "-a" in sys.argv:
-		c_vlans(cfile, 1)
+		c_vlans(cfile, 1, m)
 	else:		
-		c_vlans(cfile, 0)
+		c_vlans(cfile, 0, m)
 
